f72dd7f0fd8d0ca97b30174dd3b79e3f632051cc
Suvigya Rai <rai.suvigya3@gmail.com>
2026-06-22 11:51:51 +0530

    intro

---
+++ CPP/01.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Data Types and Variables
+    // int age=20;
+    // cout<<"Enter age: "<<endl;
+    // cin >> age;
+    // int n;
+    // cout<<"Enter number of students: "<<endl;
+    // cin>>n;
+    // if(age>18 && n>10) cout<<"Test will be conducted." << endl;
+    // else cout<<"Condition not satisfied.";
+    // // cout<<age<<endl;
+
+    // char grade='A';
+    // switch(grade){
+    //     case 'A':
+    //     cout<<"Excellent"<<endl;
+    //     break;
+    //     case 'B':
+    //     cout<<"Good"<<endl;
+    //     break;ṇ
+    //     case 'C':
+    //     cout<<"Average"<<endl;
+    //     break;
+    //     default:
+    //     cout<<"Invalid"<<endl;
+    // }
+
+    int x=10, y=20;
+    int max = (x>y)?x:y;
+    cout << max;
+}
