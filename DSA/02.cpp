e498d33a5110b4c534726b5f32b7da3c7b570b01
Suvigya Rai <rai.suvigya3@gmail.com>
2026-06-26 23:57:59 +0530

    searching

--- DSA/02.cpp
+++ DSA/02.cpp
@@ -17,4 +17,17 @@ int main(){
     }
     if(idx!=-1) cout<<"found "<<idx<< endl;
     else cout<<"not found"<<endl;
+
+
+    Binary Search 
+    int n=arr.size();
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target) start=mid+1;
+            else end=mid-1;
+        }
+        return -1;
 }
